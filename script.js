162d1673e30de0dd0131bd1c970bbe3b6373aa59
Auxile-Isaac <auxile.isaac@gmail.com>
2023-07-09 18:46:58 +0200

    made the logic functional

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 // Function to get computer's choice
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+// Function to play a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  // Convert playerSelection to lowercase for case-insensitive comparison
+  playerSelection = playerSelection.toLowerCase();
+
+  // Check for a tie
+  if (playerSelection === computerSelection.toLowerCase()) {
+    return "It's a tie!";
   }
-  
-  // Function to play a single round of Rock Paper Scissors
-  function playRound(playerSelection, computerSelection) {
-    // Convert playerSelection to lowercase for case-insensitive comparison
-    playerSelection = playerSelection.toLowerCase();
-  
-    // Check for a tie
-    if (playerSelection === computerSelection.toLowerCase()) {
-      return "It's a tie!";
-    }
-  
-    // Check all possible winning scenarios for the player
-    if (
-      (playerSelection === 'rock' && computerSelection === 'Scissors') ||
-      (playerSelection === 'paper' && computerSelection === 'Rock') ||
-      (playerSelection === 'scissors' && computerSelection === 'Paper')
-    ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-  
-    // If none of the above conditions are met, the player loses
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+
+  // Check all possible winning scenarios for the player
+  if (
+    (playerSelection === "rock" && computerSelection === "Scissors") ||
+    (playerSelection === "paper" && computerSelection === "Rock") ||
+    (playerSelection === "scissors" && computerSelection === "Paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
-  
+
+  // If none of the above conditions are met, the player loses
+  return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
